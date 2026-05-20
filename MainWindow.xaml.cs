1f8da788fd02a3dfbdd70d4db983eb7882f5dc3a
Rebeca Barella <rebecabarella13@gmail.com>
2026-05-20 16:58:35 -0300

    cria lista de nomes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Lista;
 
-/// <summary>
-///     Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    public ObservableCollection<string> nomes { get; set; } = new();
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        this.DataContext = this;
+    }
+
+    private void BtnAdicionaNome_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(tbNome.Text))
+        {
+            MessageBox.Show("Escreva um nome valido!");
+                return;
+        }
+        
+        nomes.Add(tbNome.Text);
     }
 }

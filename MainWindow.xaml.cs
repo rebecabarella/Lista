8633e278c7e39fdc46b0e46f3ae41a4fe67ba01c
Rebeca Barella <rebecabarella13@gmail.com>
2026-05-21 16:07:13 -0300

    adiciona funções de remover e encontrar nomes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,4 +24,37 @@ public partial class MainWindow : Window
         
         nomes.Add(tbNome.Text);
     }
+
+    private void BtnRemoveNome_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!nomes.Contains(tbNome.Text))
+        {
+            MessageBox.Show("Esse nome não está na lista!");
+            return;
+        }
+        var nomeEncontrado = nomes.FirstOrDefault(nomePessoa => nomePessoa.Equals(tbNome.Text, StringComparison.CurrentCultureIgnoreCase));
+        
+        
+        nomes.Remove(nomeEncontrado);
+        MessageBox.Show("Nome removido com sucesso!");
+    }
+
+    private void BtnEncontrarNomes_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(tbNome.Text))
+        {
+            MessageBox.Show("Escreva um nome valido!");
+            return;
+        }
+        lbNomes.SelectedItems.Clear();
+        string minusculo = tbNome.Text.ToLower();
+        foreach (var nome in nomes)
+        {
+            if (nome.Contains(minusculo, StringComparison.CurrentCultureIgnoreCase))
+            {
+                lbNomes.SelectedItems.Add(nome);
+            }
+        }
+
+    }
 }
